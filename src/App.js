4cbc93fbeb5976f59311af4bd26e9e6796923768
mihai-gabriel1 <mr.eu21@gmail.com>
2022-09-08 23:07:21 +0300

    Created the skeleton of my first react project.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const title = "Welcome to the new blog.";
+  const likes = 50;
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App component</h1>
+        <h2> React based </h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
