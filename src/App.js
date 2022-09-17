7a12e8bf3405f7800825091fa32222a800c95d82
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2022-09-17 17:53:49 +0300

    Added 3 blogs, that are being shown using the useState, iterating trough the array using the map method.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to my new blog.";
-
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <h2>Homepage</h2>;
+  const [blogs, setBlogs] = useState([
+    { title: "My new Website", body: "Lorem ipsum...", author: "Luigi", id: 1 },
+    { title: "Welcome party", body: "Lorem ipsum...", author: "Mario", id: 2 },
+    {
+      title: "Javascript is awesome!",
+      body: "Lorem ipsum...",
+      author: "Gabriel",
+      id: 3,
+    },
+  ]);
+  return (
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview">
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
