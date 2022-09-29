c7e361e3eaa394cd51c97e3878577f5bd41ed696
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2022-09-29 23:45:53 +0300

    A button has been added, making possible to delete any blog that has been added by clicking a button; all this by using functions as props.

--- src/Home.js
+++ src/Home.js
@@ -17,9 +17,17 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs title" />
+      <BlogList
+        blogs={blogs}
+        title="All blogs title"
+        handleDelete={handleDelete}
+      />
       <BlogList
         blogs={blogs.filter((blog) => blog.author == "Gabriel")}
         title="Gabriel's blogs"
